f0c574d1146dc5a6a125bfdb2a4344642017a0d2
Raghav Kumar <tinku.minku2803@gmail.com>
2025-04-20 05:42:20 +0530

    Update firebase.js (remove keys)

--- firebase.js
+++ firebase.js
@@ -3,15 +3,7 @@
 const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore/lite'); // Note: Use firestore/lite for client-side
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB92l7OBqQtBABrrvYdXd4nheL3efrrFlw",
-    authDomain: "eco-cart-9d811.firebaseapp.com",
-    projectId: "eco-cart-9d811",
-    storageBucket: "eco-cart-9d811.firebasestorage.app",
-    messagingSenderId: "488801897373",
-    appId: "1:488801897373:web:2885294fe80fe20072fd74",
-    measurementId: "G-XGC65R7NBV"
-  };
+
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
